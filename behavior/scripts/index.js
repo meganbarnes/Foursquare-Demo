38c0ea3e5941f10702f336909549fc64adf801f8
Megan Barnes <meg.rose.barnes@gmail.com>
2016-10-11 11:03:59 -0400

    Initial weather behavior

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,7 +1,18 @@
 'use strict'
 
-exports.handle = function handle(client) {
+const getCurrentWeather = require('./lib/getCurrentWeather')
 
+const firstOfEntityRole = function(message, entity, role) {
+  role = role || 'generic';
+
+  const slots = message.slots
+  const entityValues = message.slots[entity]
+  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+
+  return valsForRole ? valsForRole[0] : null
+}
+
+exports.handle = function handle(client) {
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
@@ -31,17 +42,67 @@ exports.handle = function handle(client) {
     }
   })
 
+  const collectCity = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().weatherCity)
+    },
+
+    extractInfo() {
+     const city = firstOfEntityRole(client.getMessagePart(), 'city')
+      if (city) {
+        client.updateConversationState({
+          weatherCity: city,
+        })
+        console.log('User wants the weather in:', city.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:prompt/weather_city')
+      client.done()
+    },
+  })
+
+  const provideWeather = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt(callback) {
+      getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
+        if (!resultBody || resultBody.cod !== 200) {
+          console.log('Error getting weather.')
+          callback()
+          return
+        }
+
+        const weatherDescription = (
+          resultBody.weather.length > 0 ?
+          resultBody.weather[0].description :
+          null
+        )
+
+        const weatherData = {
+          temperature: resultBody.main.temp,
+          condition: weatherDescription,
+          city: resultBody.name,
+        }
+
+        console.log('sending real weather:', weatherData)
+        client.addResponse('app:response:name:provide_weather/current', weatherData)
+        client.done()
+
+        callback()
+      })
+    },
+  })
+
   client.runFlow({
-    classifications: {
-			// map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
+    classifications: {},
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained]
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
     }
   })
 }
