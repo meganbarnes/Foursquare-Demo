58d5deb6f9d23e24571257ef523a391e91ffe12f
Megan Barnes <meg.rose.barnes@gmail.com>
2016-10-11 15:49:53 -0400

    Change to venues

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const getCurrentWeather = require('./lib/getCurrentWeather')
+const getVenues = require('./lib/getVenues')
 
 const firstOfEntityRole = function(message, entity, role) {
   role = role || 'generic';
@@ -42,54 +42,82 @@ exports.handle = function handle(client) {
     }
   })
 
-  const collectCity = client.createStep({
+  const collectNear = client.createStep({
     satisfied() {
-      return Boolean(client.getConversationState().weatherCity)
+      return Boolean(client.getConversationState().near)
     },
 
     extractInfo() {
-     const city = firstOfEntityRole(client.getMessagePart(), 'city')
-      if (city) {
+     const place = firstOfEntityRole(client.getMessagePart(), 'place')
+      if (place) {
         client.updateConversationState({
-          weatherCity: city,
+          near: place,
         })
-        console.log('User wants the weather in:', city.value)
+        console.log('User wants venues near:', place.value)
       }
     },
 
     prompt() {
-      client.addResponse('app:response:name:prompt/weather_city')
+      client.addResponse('app:response:name:prompt/near_place')
       client.done()
     },
   })
 
-  const provideWeather = client.createStep({
+  const collectQuery = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().query)
+    },
+
+    extractInfo() {
+     const type = firstOfEntityRole(client.getMessagePart(), 'type')
+      if (type) {
+        client.updateConversationState({
+          query: type,
+        })
+        console.log('User wants:', type.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:prompt/query_type')
+      client.done()
+    },
+  })
+
+  const provideVenues = client.createStep({
     satisfied() {
       return false
     },
 
     prompt(callback) {
-      getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
-        if (!resultBody || resultBody.cod !== 200) {
-          console.log('Error getting weather.')
+      getVenues(resultBody => {
+        if (!resultBody || resultBody.meta.code !== 200) {
+          console.log('Error getting venues.')
           callback()
           return
         }
 
-        const weatherDescription = (
-          resultBody.weather.length > 0 ?
-          resultBody.weather[0].description :
-          null
-        )
-
-        const weatherData = {
-          temperature: resultBody.main.temp,
-          condition: weatherDescription,
-          city: resultBody.name,
+        var resultLen = resultBody.response.venues.length
+        var carouselArray = []
+        for (i = 0; i < resultLen; i++) {
+          var  carouselItemData = {
+            'media_url': `http://cache.boston.com/bonzai-fba/Original_Photo/2011/11/23/pizza__1322067494_5957.jpg`,
+            'media_type': 'image/jpeg', 
+            'description': 'Pizza Place.',
+            title: resultBody.response.venues[i].name,
+            actions: [
+              {
+                type: 'link',
+                text: 'Visit page',
+                uri: resultBody.response.venues[i].url,
+              },
+            ],
+          }
+          carouselArray.push(carouselItemData)
         }
 
-        console.log('sending real weather:', weatherData)
-        client.addResponse('app:response:name:provide_weather/current', weatherData)
+        console.log('sending venues:', carouselArray)
+        client.addCarouselListResponse({ items: carouselArray })
         client.done()
 
         callback()
@@ -100,9 +128,9 @@ exports.handle = function handle(client) {
   client.runFlow({
     classifications: {},
     streams: {
-      main: 'getWeather',
+      main: 'getVenues',
       hi: [sayHello],
-      getWeather: [collectCity, provideWeather],
+      getVenues: [provideVenues],
     }
   })
 }
