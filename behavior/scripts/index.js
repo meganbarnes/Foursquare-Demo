ed5d60c6092ee234281a24fa0c58cb113225aa39
Megan Barnes <meg.rose.barnes@gmail.com>
2016-10-18 14:14:42 -0400

    move

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -162,21 +162,15 @@ exports.handle = function handle(client) {
               client.addCarouselListResponse({ items: carouselArray })
               var postbackData = client.getPostbackData()
               console.log("POstback data", postbackData)
-              if (postbackData != null) {
-                client.updateConversationState({
-                  near: {
-                    value: resultBody.resourceSets[0].resources[0].point.coordinates[0].toString()+','+resultBody.resourceSets[0].resources[0].point.coordinates[1].toString(),
-                    raw_value: client.getConversationState().near.raw_value,
-                    canonicalized: client.getConversationState().near.canonicalized,
-                    parsed: client.getConversationState().near.parsed,
-                  },
-                  convertedNear: true,
-                })
-              } else {
-                client.updateConversationState({
-                  convertedNear: false,
-                })
-              }
+              client.updateConversationState({
+                near: {
+                  value: resultBody.resourceSets[0].resources[0].point.coordinates[0].toString()+','+resultBody.resourceSets[0].resources[0].point.coordinates[1].toString(),
+                  raw_value: client.getConversationState().near.raw_value,
+                  canonicalized: client.getConversationState().near.canonicalized,
+                  parsed: client.getConversationState().near.parsed,
+                },
+                convertedNear: true,
+              })
               console.log('conv state:', client.getConversationState())
               client.expect('getVenues', ['affirmative', 'provide/near_place'])
               client.expect('reset', ['decline'])
